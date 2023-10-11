4e9fe32185105260dbc19b9fb38a969df3f4ec8b
AcheampongAndy <acheampongandrews1999@gmail.com>
2023-10-11 11:48:46 +0000

    factors combine and order

--- Data_Wrangle:_strings-factors/14_values_change_add_drop.R
+++ Data_Wrangle:_strings-factors/14_values_change_add_drop.R
@@ -5,3 +5,19 @@ rm(list = ls())
 library(forcats)
 library(ggplot2)
 library(dplyr)
+library(forcats)
+
+# lets create factor variables
+df <- mpg %>%
+  mutate_at(.vars = c("manufacturer", "model", "trans", "class"),
+            .funs = as_factor)
+
+# fct_recode () 
+
+## first lets pull levels and add country of origin
+df %>% pull(manufacturer) %>% fct_count()
+
+levels.country <- tibble(
+  ~company,       ~country,
+  
+)
